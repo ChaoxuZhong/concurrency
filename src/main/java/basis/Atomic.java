bc942207f0ed6b5b41b75b1903bcf827756fbb00
Chaoxu Zhong <chaoxu@Chaoxus-MacBook-Pro.local>
2020-06-18 19:41:47 +0800

    create concurrency

---
+++ src/main/java/basis/Atomic.java
@@ -0,0 +1,39 @@
+package basis;
+
+public class Atomic {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(Atomic.calc());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static long count = 0;
+
+    private void add10K() {
+        int idx = 0;
+        while (idx++ < 3000) {
+            count += 1;
+        }
+    }
+
+    public static long calc() throws InterruptedException {
+        final Atomic test = new Atomic();
+        // 创建两个线程，执行add()操作
+        Thread th1 = new Thread(() -> {
+            test.add10K();
+        });
+        Thread th2 = new Thread(() -> {
+            test.add10K();
+        });
+        // 启动两个线程
+        th1.start();
+        th2.start();
+        // 等待两个线程执行结束
+        th1.join();
+        th2.join();
+        return count;
+    }
+}
